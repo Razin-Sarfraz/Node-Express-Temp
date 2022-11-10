bb74e3981a9352334eb282a951a4c280a055fd6f
razin.sarfraz <razin.sarfraz@rnssol.com>
2022-11-10 15:38:02 +0500

    2nd commit

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const server = http
-  .createServer((req, res) => {
-    console.log("Starting server");
-    res.end("Hello world")
-  })
-  .listen(5000);
+app.use(express.static('./public/'))
+
+app.get('/', (req, res) => {
+res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+});
+
+app.get('*', (req, res) => {
+res.status(404).send('Resource not found')
+});
+
+app.listen(5000, () => {
+  console.log("Server listeneing port 5000....");
+});
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
