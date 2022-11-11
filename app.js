35dd45e15fbf45dae22799e16f1641e14ba4db48
razin.sarfraz <razin.sarfraz@rnssol.com>
2022-11-11 09:50:33 +0500

    3rd commit

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
+const { products } = require("./data");
 const app = express();
 
-app.use(express.static('./public/'))
-
-app.get('/', (req, res) => {
-res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello</h1><a href="/api/products">products</a>`);
+  // adding to static
+  // SSR
 });
 
-app.get('*', (req, res) => {
-res.status(404).send('Resource not found')
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
+
+app.get("/api/products/:productId", (req, res) => {
+  console.log(req.params);
+  const { productId } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productId)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does not exist");
+  }
+  res.json(singleProduct);
+});
+
+// app.get('*', (req, res) => {
+// res.status(404).send('Resource not found')
+// });
 
 app.listen(5000, () => {
   console.log("Server listeneing port 5000....");
